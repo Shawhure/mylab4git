55fb9fafb30138b1f5011aa7e3378cf3c2144444
Shawhure <1445663758@qq.com>
2015-11-11 10:45:07 +0800

    fix three bugs

--- index.wsgi
+++ index.wsgi
@@ -5,3 +5,6 @@ import sae
 from pickbooksup import wsgi
   
 application = sae.create_wsgi_app(wsgi.application)  
+
+#fix a bug 
+#bug's id=2
--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
+import time #fix bug 3
+
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pickbooksup.settings")
@@ -8,3 +10,4 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+
